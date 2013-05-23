abed46222271743b95325ed308481f0a406a9f43
Sean-Paul Fenton <seanpaulfenton@gmail.com>
2013-05-23 14:06:06 -0400

    Add a Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,69 @@
 require 'spec_helper'
 
-describe "Static pages" do
+    describe "Static pages" do
 
-  describe "Home page" do
+      describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+        it "should have the content 'Sample App'" do
+          visit '/static_pages/home'
+          page.should have_content('Sample App')
+        end
+
+        it "should should have the title 'Home'" do
+        	visit '/static_pages/home'
+        	page.should have_selector('title',
+        		:text => "Ruby on Rails Tutorial Sample App | Home")
+        end
+
+      end
+
+    describe "Help page" do
+
+    	it "should have the content 'Help'" do
+    		visit '/static_pages/help'
+    		page.should have_content('Help')
+    	end
+
+        it "should should have the title 'Help'" do
+        	visit '/static_pages/help'
+        	page.should have_selector('title',
+        		:text => "Ruby on Rails Tutorial Sample App | Help")
+        end
+
     end
 
-    it "should should have the title 'Home'" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Home")
-    end
+    	describe "About page" do
+    		it "should should have the content 'About Us'" do
+    			visit '/static_pages/about'
+    			page.should have_content('About')
+    		end
 
-  end
+            it "should should have the title 'About'" do
+                visit '/static_pages/about'
+                page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About")
+            end
+    	
+        end
 
-describe "Help page" do
 
-	it "should have the content 'Help'" do
-		visit '/static_pages/help'
-		page.should have_content('Help')
-	end
+    describe "Contact Page" do
+        it "should have the content 'Contact'" do
+            visit '/static_pages/contact'
+            page.should have_content('Contact')
+        end
 
-    it "should should have the title 'Help'" do
-    	visit '/static_pages/help'
-    	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+        it "should have the title 'Contact'" do
+            visit '/static_pages/contact'
+            page.should have_selector('title',
+                :text => 'Ruby on Rails Tutorial Sample App | Contact')
+        end
 
-end
+        it "should have the header 'Contact'" do
+            visit '/static_pages/contact'
+            page.should have_selector('h1', :text => 'Contact')
+        end
 
-	describe "About page" do
-		it "should should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_content('About')
-		end
-	end
-
-    it "should should have the title 'About'" do
-    	visit '/static_pages/about'
-    	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | About")
     end
 
 end
